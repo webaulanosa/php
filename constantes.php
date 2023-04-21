f00d8dbeb01865f0588962839b14c7d8e2f731ae
AlejandroSoftwareOne <ext.alejandro.munoz@softwareone.com>
2023-04-21 10:44:09 +0200

    WEB PHP

--- constantes.php
+++ constantes.php
@@ -4,3 +4,7 @@ define('PI', 3.1416);
 
 echo "El valor de la constante es " . PI;
 
+// OJO que si queremos imprimir la constante dentro de alguna etiqueta html tenemos que concatenar
+echo "<h2>PI</h2>";  // Imprimi PI en texto plano
+echo "<h2>". PI ."</h2>";  // ASÍ SI FUNCIONA !!
+
